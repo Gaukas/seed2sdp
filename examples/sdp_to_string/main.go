afb932f692e0230bdb6c56e4ba26e7cfaec7d554
Gaukas <gaukaswang@gmail.com>
2021-04-21 15:29:15 -0600

    Updated Examples
    Examples are now working with new implementation of SDP struct.

--- examples/sdp_to_string/main.go
+++ examples/sdp_to_string/main.go
@@ -18,26 +18,46 @@ func main() {
 		SetCandidateType(s2s.Srflx)
 
 	sampleSDP := s2s.SDP{
-		SDPType: "offer",
-		GlobalLines: s2s.SdpGlobal{
-			SessionId:   5615412156857050866,
-			SessionVer:  1614192136,
-			NetworkType: s2s.IN,
-			IpaddrType:  s2s.IP4,
-			UnicastAddr: net.IPv4(0, 0, 0, 0),
-			// SessionName: "",
-			// StartingTime: 0,
-			// EndingTime: 0,
-			GroupBundle: []string{"0"},
-			// Payload: "",
+		SDPType:    "offer",
+		Malleables: s2s.NewSDPMalleables(),
+		Medias: []s2s.SDPMedia{
+			{
+				MediaType:   "application",
+				Description: "9 UDP/DTLS/SCTP webrtc-datachannel",
+			},
 		},
-		Payload:     `m=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\nc=IN IP4 0.0.0.0\r\na=setup:actpass\r\na=mid:0\r\na=sendrecv\r\na=sctp-port:5000`,
-		Fingerprint: webrtc.DTLSFingerprint{Algorithm: "sha-256", Value: `70:A8:3B:77:1C:7F:A5:EB:DB:D3:57:D7:7F:54:CF:0F:E0:45:F0:7D:60:25:7A:D2:38:64:C3:71:F2:A3:76:A1`},
+		Attributes: []s2s.SDPAttribute{
+			{
+				Key:   "group",
+				Value: "BUNDLE 0",
+			},
+			{
+				Key:   "setup",
+				Value: "actpass",
+			},
+			{
+				Key:   "mid",
+				Value: "0",
+			},
+			{
+				Value: "sendrecv", // Transceivers
+			},
+			{
+				Key:   "sctp-port",
+				Value: "5000",
+			},
+		},
+		Fingerprint: webrtc.DTLSFingerprint{
+			Algorithm: "sha-256",
+			Value:     `70:A8:3B:77:1C:7F:A5:EB:DB:D3:57:D7:7F:54:CF:0F:E0:45:F0:7D:60:25:7A:D2:38:64:C3:71:F2:A3:76:A1`,
+		},
 		IceParams: s2s.ICEParameters{
 			UsernameFragment: "bmcKQgPzKgnCMKPL",
 			Password:         "BPwAktFHQCXCbPTjyQJrXnsBUMRgxDUT",
 		},
-		IceCandidates: []s2s.ICECandidate{testCandidate},
+		IceCandidates: []s2s.ICECandidate{
+			testCandidate,
+		},
 	}
 
 	fmt.Println(sampleSDP.String())
